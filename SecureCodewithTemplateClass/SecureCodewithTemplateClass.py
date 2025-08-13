6bbf16ba04f5cfacb971fe62a5b66dd337419ebb
Owner <Owner@DESKTOP-BT3BM35>
2025-08-13 16:01:29 -0400

    hashlib

---
+++ SecureCodewithTemplateClass/SecureCodewithTemplateClass.py
@@ -0,0 +1,32 @@
+
+from string import Template
+
+CONFIG = {
+
+    "API_KEY": "'you've just exposed your secret_key'"
+}
+class User:
+
+    name = ""
+    email = ""
+
+    def __init__(self, name, email):
+
+        self.name = name
+        self.email = email
+
+    def __str__(self):
+
+        return self.name
+        
+if __name__ == '__main__':
+    name = input()
+    email = input()
+    
+    user = User(name, email)
+    
+    # FIXME:  Here is where you want to use the template class
+    name_template = Template("Hello, my name is $name.")
+    greeting = name_template.substitute(name=name)
+
+    print(greeting)
